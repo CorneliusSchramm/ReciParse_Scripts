871ba00423c9a1a31c4ee8752fb8066426bf597e
CSchramm <cornelius.schramm@student.unisg.ch>
2020-10-13 11:09:06 +0200

    Setup

--- test.py
+++ test.py
@@ -1,2 +1,40 @@
-coco_path_to_data = r"C:\Users\CocoL\Universität St.Gallen\STUD-Capstoneproject Tell 6 - General\Coding\1-Data"
-print(coco_path_to_data)
+################################################################################################################
+# Setup and Package Import
+################################################################################################################
+
+# --------------------------------------------------------------------------------------------------------------
+# Importing
+# --------------------------------------------------------------------------------------------------------------
+
+import os
+# import pandas as pd
+# import numpy as np
+# import pickle
+# import csv
+# import json
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# sns.set()
+# pd.options.display.float_format = '{:.2f}'.format
+
+# --------------------------------------------------------------------------------------------------------------
+# Getting Paths and Working Correct
+# --------------------------------------------------------------------------------------------------------------
+
+# print(os.getcwd()) 
+# os.chdir("Change to your path if necessary")
+coco_path = r"C:\Users\CocoL\Universität St.Gallen\STUD-Capstoneproject Tell 6 - General\Coding"
+jona_path = "some ugly mac book path"
+keule_path = "some ugly mac book path"
+leo_path = "some ugly mac book path"
+
+################################################################################################################
+# Loading Data
+################################################################################################################
+
+test_file_path = os.path.join(coco_path,"1-Data", 'test_data.txt')
+
+with open(test_file_path) as f:
+    first_record = f.readline() 
+    
+print(first_record)
