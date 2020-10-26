245398dab11c242b1b7eb0f55be229089324285a
leonidasbothmer <44315917+leonidasbothmer@users.noreply.github.com>
2020-10-26 12:24:45 +0100

    mainly vocab and similarity
    vocab contains code that transforms the recipe instructions into a list of tokens (without stop words). ATM cut to a few recipes only resulting in around 400 uniqu words.
    Similarity doc creates a similarity matrix for all the words of previously mentioned token list to ultimately use to eliminate synoynms

--- ingredients/ingredient_functions.py
+++ ingredients/ingredient_functions.py
@@ -1,13 +1,14 @@
 import spacy
 
 # Clean ingredients from stop words
-def _filter(token):
+def _filter(token, only_nouns):
     if len(token) < 2:
         return False
     if token.is_stop:
         return False
-    if token.text[0].islower(): # Schmeisst alle kleingeschriebenen Wörter raus -- gut? Alternative?
-        return False
+    if only_nouns:
+        if token.text[0].islower(): # Schmeisst alle kleingeschriebenen Wörter raus -- gut? Alternative?
+            return False
     if token.is_digit:
         return False
     if token.like_num:
@@ -19,3 +20,16 @@ def _clean(text):
     text = text.replace("(", "")
     text = text.split("/")[0]
     return text
+
+
+# def normalize(token, lowercase, lemma, remove_stopwords):
+    # Make lowercase
+    # if lowercase:
+    #     token. 
+
+    # Lemmatize
+    # if lemma:
+    
+    # Remove puncts --> is it included in stop words?
+
+    # return 
