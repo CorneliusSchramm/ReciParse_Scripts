08c3f3b94e5f104756993c6c86e266863bec4e6a
JEbner97 <jonathan.ebner@student.unisg.ch>
2020-11-17 22:01:02 +0100

    updates

--- test_area/jonathan/database_tests.py
+++ test_area/jonathan/database_tests.py
@@ -11,5 +11,3 @@ all_sessions = db.sessions
 print("Names of all Datasets:", all_annotations)
 print("Amount datasets:", len(db))
 print("All sessions:", all_sessions)
-
-print(db.get_dataset("rec-anno5"))
