165e9a6b1a2e00a00152e975da4617b842609a16
leonidasbothmer <44315917+leonidasbothmer@users.noreply.github.com>
2020-11-23 11:48:08 +0100

    Added Relations project
    and some minor changes

---
+++ json_transformation/bad_spans.py
@@ -0,0 +1,18 @@
+from prodigy.components.db import connect
+db = connect()
+examples = db.get_dataset("batch1jonathan30")
+filtered_examples = []
+for eg in examples:
+    if "spans" in eg:
+        new_spans = []
+        for span in eg["spans"]:
+            if "start" not in span or "end" not in span:
+                print("Found bad span:", span)
+            else:
+                new_spans.append(span)
+        eg["spans"] = new_spans
+    filtered_examples.append(eg)
+
+# Add filtered examples to new dataset
+db.add_dataset("myDataset2_filtered")
+db.add_examples(filtered_examples, ["batch1jonathan30_f"])
