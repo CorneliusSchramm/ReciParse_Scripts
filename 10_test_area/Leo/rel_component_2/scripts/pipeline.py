9c7a9fd165b4791dabb7e28f01e0bdb9c25ee414
leonidasbothmer <44315917+leonidasbothmer@users.noreply.github.com>
2020-11-28 19:17:26 +0100

    functioning two part pipeline

--- 10_test_area/Leo/rel_component_2/scripts/pipeline.py
+++ 10_test_area/Leo/rel_component_2/scripts/pipeline.py
@@ -1,10 +1,10 @@
-import random
+# import random
 import pandas as pd
 import typer
 from pathlib import Path
 import spacy
-from spacy.tokens import DocBin, Doc
-from spacy.training.example import Example
+# from spacy.tokens import DocBin, Doc
+# from spacy.training.example import Example
 
 # make the factory work
 from rel_pipe import make_relation_extractor, score_relations
@@ -12,75 +12,48 @@ from rel_pipe import make_relation_extractor, score_relations
 # make the config work
 from rel_model import create_relation_model, create_classification_layer, create_instances, create_tensors
 
-print("Hi")
-
-# input verändern: Ein oder mehrere Rezepte
-# nlp pipeline component die tokenized und ner macht
 
 
 def main(ner_pipeline: Path, trained_pipeline: Path, input_data: Path, threshold: float = 0.8):
+
+    # Load input data into a pandas dataframe and convert to list
+    data = pd.read_json(input_data)["text"].to_list()
     
-    data = pd.read_json(input_data)
-    print(data)
-    test = data["text"][0]
+    # Load pipelines
+    ner_nlp = spacy.load(ner_pipeline)
+    rel_nlp = spacy.load(trained_pipeline)
 
-    print("--> Load NER Model")
-    nlp = spacy.blank('de')
+    # Objekt, das alle Docs inklusive Predictions speichert
+    preds_list = []
 
-    print("Pipeline:",nlp.pipeline)
+    # Iterate over recipes
+    for recipe in data:
 
-    # nlp = spacy.load("/Users/Leonidas/Universität St.Gallen/STUD-Capstoneproject Tell 6 - General/02-Coding/03-Models/Custom Tokenizer/de_custom_v2")
+        # Get NE predictions
+        pred = ner_nlp(recipe)
+        # print([(ent, ent.label_, ent.start) for ent in pred.ents])
 
+        # Get Relation predictions
+        for name, proc in rel_nlp.pipeline:
+            pred = proc(pred)
 
-    # ner = nlp.add_pipe("ner")
+        # List of start tokens of "V"-entities
+        v_list = [ent.start for ent in pred.ents if ent.label_ == "V" ]
+        
+        # Printing relevant relations
+        for item in pred._.rel.items():
+            if item[0][0] in v_list:
+                if max(item[1].values()) > threshold:
+                    print(item[0],max(item[1], key=item[1].get),max(item[1].values()))
+                else: 
+                    print("No probable relation.")
 
-    # print("--> Apply Model")
-    # ner = nlp(test)
+        # Append predicted doc to list of preds_list
+        preds_list.append(pred)
 
-    # print(doc.ents)
+        print("Recipe predicted")
 
-    # (Tokenizer) + Pipeline [  NER, Tok2Vec, REL ]
-
-
-
-    # nlp = spacy.load(trained_pipeline)
-
-    # doc_bin = DocBin(store_user_data=True).from_disk(test_data)
-    # docs = doc_bin.get_docs(nlp.vocab)
-
-    # # Iterate through annotated test recipes
-    # for gold in docs:
-    #     # Create predictions with our trained pipeline and save them under var "pred"
-    #     pred = Doc(
-    #         nlp.vocab,
-    #         words=[t.text for t in gold],
-    #         spaces=[t.whitespace_ for t in gold],
-    #     )
-
-    #     # Set the entities of the "pred" object equal to the annotated ones
-    #     pred.ents = gold.ents
-
-    #     # Print out the label for each entity
-    #     for ent in pred.ents:
-    #     	print((ent.text, ent.label_, "start:",ent.start,"end:",ent.start ))
-    #     	break
-
-    #     # nlp.add_pipe("ner", first=True)
-    #     print(nlp.pipeline)
-
-    #     # Ich glaube, hier werden die pipeline components andgewandt
-    #    	for name, proc in nlp.pipeline:
-    #         pred = proc(pred)
-    #         print(name)
-
-    #     for item in pred._.rel.items():
-    #     	print(item)
-    #     	break
-
-    #     break
-
-
-        # ANSWER KEY
+    return preds_list
 
    
 
--- 10_test_area/Leo/rel_component_2/scripts/prodigy_format.py
+++ 10_test_area/Leo/rel_component_2/scripts/prodigy_format.py
@@ -1,3 +1,19 @@
+# import random
+import pandas as pd
+import typer
+from pathlib import Path
+import spacy
+# from spacy.tokens import DocBin, Doc
+# from spacy.training.example import Example
+
+# make the factory work
+from rel_pipe import make_relation_extractor, score_relations
+
+# make the config work
+from rel_model import create_relation_model, create_classification_layer, create_instances, create_tensors
+
+
+
 colors_dict = {
 	"Arg0": "#c5bdf4",
 	"Arg1": "#ffd882",
