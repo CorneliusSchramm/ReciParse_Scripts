d4260c447b9b850083b2c52bd634e9ce0da9b1a1
jhoff8 <jona.david.hoffmann@googlemail.com>
2020-11-28 17:47:47 +0100

    xx

--- 10_test_area/hoffinho/04_Relations/02_new/rel_component_2/scripts/custom_pipeline.py
+++ 10_test_area/hoffinho/04_Relations/02_new/rel_component_2/scripts/custom_pipeline.py
@@ -1,11 +1,25 @@
 import spacy
-from rel_pipe import RelationExtractor
+"""from rel_pipe import RelationExtractor
 from rel_pipe import make_relation_extractor
 from rel_pipe import relation_extactor
+from pathlib import Path
+from spacy.util import load_model_from_path"""
 
-nlp = spacy.load("en_core_web_sm")
+from thinc.api import Config
+"""from spacy.language import Language
+from rel_model import create_relation_model, create_classification_layer, create_instances, create_tensors
+from spacy.vocab import Vocab"""
+from spacy.lang.en import English
 
-print(nlp.pipeline)
+
+#nlp = spacy.load("en_core_web_sm")
+
+config = Config().from_disk("/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/hoffinho/04_Relations/02_new/rel_component_2/configs/rel_tok2vec.cfg")
+nlp = English().from_config(config)
+
+nlp.add_pipe("ner", first=True)
+
+
 print(nlp.pipe_names)
 
-nlp.add_pipe("relation_extactor")
+confs = nlp.config
--- 10_test_area/hoffinho/04_Relations/02_new/rel_component_2/scripts/pipeline_j_ner.py
+++ 10_test_area/hoffinho/04_Relations/02_new/rel_component_2/scripts/pipeline_j_ner.py
@@ -1,5 +1,7 @@
 import spacy
+from spacy import Language
 
+print(type(Language.factory))
 
 test_sentence = "Den Seeteufel in 2-3 cm große Würfel, den Schnittlauch in feine Röllchen schneiden.Die Butter zerlassen und die Fischwürfel kurz anziehen lassen. Mit Weißwein ablöschen und zugedeckt 2-3 Minuten ziehen lassen."
 
