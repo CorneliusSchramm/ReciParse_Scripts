ab4d7ba8b6a0aa23846ebeb6ee5531e51a44a4ee
jhoff8 <jona.david.hoffmann@googlemail.com>
2020-11-29 19:42:30 +0100

    xx

---
+++ 10_test_area/hoffinho/04_Relations/02_new/rel_component_3/scripts/j_pipeline_ner.py
@@ -0,0 +1,14 @@
+import spacy
+from spacy import Language
+
+print(type(Language.factory))
+
+test_sentence = "Den Seeteufel in 2-3 cm große Würfel, den Schnittlauch in feine Röllchen schneiden.Die Butter zerlassen und die Fischwürfel kurz anziehen lassen. Mit Weißwein ablöschen und zugedeckt 2-3 Minuten ziehen lassen."
+
+# load existing spacy model 
+nlp = spacy.load("de_core_news_sm")
+
+print(nlp.pipeline)
+print(nlp.pipe_names)
+
+print(nlp.config.to_str())
