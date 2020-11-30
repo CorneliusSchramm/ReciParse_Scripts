4ddb8ad4be52a22b0008191c27c5786d5b1861e4
jhoff8 <jona.david.hoffmann@googlemail.com>
2020-11-30 14:13:40 +0100

    to 3d

--- 10_test_area/hoffinho/04_Relations/02_new/rel_component_4/scripts/j_custom_pipeline.py
+++ 10_test_area/hoffinho/04_Relations/02_new/rel_component_4/scripts/j_custom_pipeline.py
@@ -1,4 +1,4 @@
-import spacy
+import spacy import 
 from thinc.api import Config
 from spacy.lang.en import English
 
@@ -8,13 +8,18 @@ from rel_pipe import make_relation_extractor, score_relations
 # make the config work
 from rel_model import create_relation_model, create_classification_layer, create_instances, create_tensors
 
-nlp = spacy.load("/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/hoffinho/04_Relations/02_new/rel_component_2/trainig/model-best")
+nlp = spacy.load("/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/hoffinho/04_Relations/02_new/rel_component_4/training/model-best")
 
 print(nlp.pipe_names)
 
-doc = nlp("Hallo das ist ein test")
+doc = nlp("Zwiebeln mit dem scharfen Messer schneiden und zum Curry hinzugeben.")
 
-print(doc._.rel)
+nlp.update()
+
+for rel in doc._.rel: 
+    print(rel)
+
+print(doc.ents)
 
 confs = nlp.config
 
