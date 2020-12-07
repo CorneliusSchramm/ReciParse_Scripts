f7c991523bcc16bcb7b1777d717233cc255c04fd
jhoff8 <jona.david.hoffmann@googlemail.com>
2020-12-07 15:49:35 +0100

    test only verb combs

--- 10_test_area/hoffinho/04_Relations/02_new/rel_component_3/scripts/parse_data.py
+++ 10_test_area/hoffinho/04_Relations/02_new/rel_component_3/scripts/parse_data.py
@@ -48,6 +48,7 @@ def main(json_loc: Path, train_file: Path, dev_file: Path, test_file: Path, dev_
                     spans = example["spans"]    #list of dicts containing entities
                     entities = []
                     span_end_to_start = {}
+                    ents_dict = {}
                     for span in spans:          #every detected span
                         entity = doc.char_span(
                             span["start"], span["end"], label=span["label"]
@@ -55,13 +56,20 @@ def main(json_loc: Path, train_file: Path, dev_file: Path, test_file: Path, dev_
                         span_end_to_start[span["token_end"]] = span["token_start"] #end_token of span as key for start_token (start token = wievielter token in doc)
                         entities.append(entity)                 #appended to list
                         span_starts.add(span["token_start"])    #added to set
+                        ents_dict[span["token_start"]] = span["label"]
                     doc.ents = entities                         #entity list assigned as doc entites
 
                     # Parse the relations
                     rels = {}
+                    print(ents_dict)
+
                     for x1 in span_starts:
-                        for x2 in span_starts:
-                            rels[(x1, x2)] = {}         #every possible span combination becomes key for individual dict (1,1), (1,2) ...
+                        if ents_dict[x1] == "V":
+                            print("is verb")
+                            for x2 in span_starts:
+                                if ents_dict[x2]!= "V":
+                                    rels[(x1, x2)] = {}         #every possible span combination becomes key for individual dict (1,1), (1,2) ...
+        
                     relations = example["relations"]    #relations is list of dicts
                     for relation in relations:
                         # the 'head' and 'child' annotations refer to the end token in the span
@@ -76,11 +84,14 @@ def main(json_loc: Path, train_file: Path, dev_file: Path, test_file: Path, dev_
 
                     # The annotation is complete, so fill in zero's where the data is missing
                     for x1 in span_starts:
-                        for x2 in span_starts:
-                            for label in MAP_LABELS.values():           #for every label
-                                if label not in rels[(x1, x2)]:         #if label isn't assigned to span combination
-                                    neg += 1                            
-                                    rels[(x1, x2)][label] = 0.0         #span combination with label as key gets 0 as value
+                        if ents_dict[x1] == "V":
+                            for x2 in span_starts:
+                                if ents_dict[x2]!= "V":
+                                    for label in MAP_LABELS.values():           #for every label
+                                        if label not in rels[(x1, x2)]:         #if label isn't assigned to span combination
+                                            neg += 1                            
+                                            rels[(x1, x2)][label] = 0.0         #span combination with label as key gets 0 as value
+                    print(rels)
                     doc._.rel = rels                                    # rels = {(1,1): {Arg0 : 1, Arg1 : 0, Arg : 0}, (1,2): {Arg0 : 0, ...}}
 
                     # only keeping documents with at least 1 positive case (if doc isn't annotated relations = empty list)
