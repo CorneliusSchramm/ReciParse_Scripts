49aa047f0417886d259773e2e720651935a8622d
jhoff8 <jona.david.hoffmann@googlemail.com>
2020-12-09 00:13:22 +0100

    first try combining all

--- 10_test_area/hoffinho/04_Relations/02_new/rel_component (all)/scripts/custom_pipe_j2.py
+++ 10_test_area/hoffinho/04_Relations/02_new/rel_component (all)/scripts/custom_pipe_j2.py
@@ -4,7 +4,7 @@ import typer
 from pathlib import Path
 import spacy
 from spacy.tokens import DocBin, Doc
-from spacy.training.example import Example
+#from spacy.training.example import Example
 import json
 
 # make the factory work
@@ -13,11 +13,11 @@ from rel_pipe import make_relation_extractor, score_relations
 # make the config work
 from rel_model import create_relation_model, create_classification_layer, create_instances, create_tensors
 
-def main(ner_pipeline="/Users/jhoff/Desktop/model-best-ner", t_p_arg="/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/hoffinho/04_Relations/02_new/rel_component/training/model-best", 
-t_p_arg0="",t_p_arg1="", input_data="/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/hoffinho/04_Relations/02_new/rel_component/assets/input1.json", threshold=0.02):
+def main(ner_pipeline="/Users/jhoff/Desktop/model-best-ner", t_p_arg="/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/hoffinho/04_Relations/02_new/rel_component (arg)/training/model-last", 
+t_p_arg0="/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/hoffinho/04_Relations/02_new/rel_component (arg0)/training/model-last",t_p_arg1="/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/hoffinho/04_Relations/02_new/rel_component (arg1)/training/model-last", input_data="/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/hoffinho/04_Relations/02_new/rel_component (all)/assets/input1.json", threshold=0.02):
     
     data = pd.read_json(input_data)["text"].to_list()
-    
+
     # Load pipelines
     ner_nlp = spacy.load(ner_pipeline)
     rel_a_nlp = spacy.load(t_p_arg)
@@ -55,7 +55,7 @@ t_p_arg0="",t_p_arg1="", input_data="/Users/jhoff/Desktop/ReciParse_Scripts/10_t
         prob = arg_dict[max_key]
 
         if max_key == "Arg":
-            if prob >= 0.02:
+            if prob >= 0.4:
                 rel_dict[start_token_head]["assigned"].append((text_child, labl_child, max_key, prob))
         else:
             if prob >= 0.000002:
@@ -73,7 +73,7 @@ t_p_arg0="",t_p_arg1="", input_data="/Users/jhoff/Desktop/ReciParse_Scripts/10_t
         prob = arg_dict[max_key]
 
         if max_key == "Arg0":
-            if prob >= 0.02:
+            if prob >= 0.05:
                 rel_dict[start_token_head]["assigned"].append((text_child, labl_child, max_key, prob))
         else:
             if prob >= 0.000002:
@@ -91,7 +91,7 @@ t_p_arg0="",t_p_arg1="", input_data="/Users/jhoff/Desktop/ReciParse_Scripts/10_t
         prob = arg_dict[max_key]
 
         if max_key == "Arg1":
-            if prob >= 0.02:
+            if prob >= 0.9:
                 rel_dict[start_token_head]["assigned"].append((text_child, labl_child, max_key, prob))
         else:
             if prob >= 0.000002:
