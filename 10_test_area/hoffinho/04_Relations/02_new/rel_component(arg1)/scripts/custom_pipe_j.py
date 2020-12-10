a404a9288d989d1dd3becfdc30608efcf86bebb8
jhoff8 <jona.david.hoffmann@googlemail.com>
2020-12-10 09:57:19 +0100

    xx

--- 10_test_area/hoffinho/04_Relations/02_new/rel_component(arg1)/scripts/custom_pipe_j.py
+++ 10_test_area/hoffinho/04_Relations/02_new/rel_component(arg1)/scripts/custom_pipe_j.py
@@ -13,14 +13,12 @@ from rel_pipe import make_relation_extractor, score_relations
 # make the config work
 from rel_model import create_relation_model, create_classification_layer, create_instances, create_tensors
 
-def main(ner_pipeline="/Users/jhoff/Desktop/model-best-ner", t_p_arg="/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/hoffinho/04_Relations/02_new/rel_component/training/model-best", 
-t_p_arg10="", input_data="/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/hoffinho/04_Relations/02_new/rel_component/assets/input1.json", threshold=0.02):
+def main(ner_pipeline="/Users/jhoff/Desktop/model-best-ner", t_p_arg="./training/model-best", input_data="./assets/input1.json", threshold=0.02):
     
     data = pd.read_json(input_data)["text"].to_list()
     # Load pipelines
     ner_nlp = spacy.load(ner_pipeline)
     rel_a_nlp = spacy.load(t_p_arg)
-    rel_a10_nlp = spacy.load(t_p_arg10)
     
     ner_docs = []
     for recipe in data:
@@ -33,8 +31,8 @@ t_p_arg10="", input_data="/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/ho
     for name, proc in rel_a_nlp.pipeline:
         doc_rel_a = proc(ner_docs[0])
     
-    for name, proc in rel_a10_nlp.pipeline:
-        doc_rel_a10 = proc(ner_docs[0])
+    # for name, proc in rel_a10_nlp.pipeline:
+    #     doc_rel_a10 = proc(ner_docs[0])
 
     rel_dict = {ent.start : {"verb": ent, "assigned": []} for ent in ner_docs[0].ents if ent.label_ == "V"}
 
@@ -56,7 +54,7 @@ t_p_arg10="", input_data="/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/ho
             if prob >= 0.000002:
                 rel_dict[start_token_head]["assigned"].append((text_child, labl_child, max_key, prob))
     
-    for item in doc_rel_a10._.rel.items():
+    """for item in doc_rel_a10._.rel.items():
         
         #pull rel information
         start_token_head, start_token_child, arg_dict = [item[0][0], item[0][1], item[1]]
@@ -75,7 +73,7 @@ t_p_arg10="", input_data="/Users/jhoff/Desktop/ReciParse_Scripts/10_test_area/ho
                 rel_dict[start_token_head]["assigned"].append((text_child, labl_child, max_key, prob)) 
         else:
             if prob >= 0.000002:
-                rel_dict[start_token_head]["assigned"].append((text_child, labl_child, max_key, prob))
+                rel_dict[start_token_head]["assigned"].append((text_child, labl_child, max_key, prob))"""
     
     # print(rel_dict)
 
