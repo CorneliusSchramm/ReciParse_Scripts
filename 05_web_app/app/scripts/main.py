691fdd475d507cf1434ecbeb0ffe41f9a21fe2a0
JEbner97 <jonathan.ebner@student.unisg.ch>
2021-01-07 21:02:45 +0100

    dscv

--- 05_web_app/app/scripts/main.py
+++ 05_web_app/app/scripts/main.py
@@ -32,16 +32,12 @@ import custom_functions
 # rel_nlp = spacy.load(r"/Users/leonidas/OneDrive - Universität St.Gallen/General/02-Coding/03-Models/Relations/16-12_relations_tok2vec")
 # rel_nlp = spacy.load(r"/Users/leonidas/OneDrive - Universität St.Gallen/General/02-Coding/04-performance/rel-trf-01_04-coco/training/model-best")
 #make the factory work
-from rel_pipe import make_relation_extractor, score_relations
-
-#make the config work
-from rel_model import create_relation_model, create_classification_layer, create_instances, create_tensors
 
 # Jonathans paths
 ner_nlp = spacy.load(r"/Users/jonathanebner/Universität St.Gallen/STUD-Capstoneproject Tell 6 - General/02-Coding/03-Models/NER trained on nightly/ner-cb1-159-15-12")
 # # rel_nlp = spacy.load(r"C:\Users\CocoL\Universität St.Gallen\STUD-Capstoneproject Tell 6 - General\02-Coding\03-Models\Relations\0-trained-on-djx\model-best")
 rel_nlp = spacy.load(r"/Users/jonathanebner/Universität St.Gallen/STUD-Capstoneproject Tell 6 - General/02-Coding/03-Models/Relations/16-12_relations_tok2vec")
-# rel_nlp = spacy.load(r"/Users/jonathanebner/Desktop/app_transformer/model/rel_trans")
+#rel_nlp = spacy.load(r"/Users/jonathanebner/Desktop/app_transformer/model/rel_trans")
 print("models loaded")
 
 
